cf3ee9ed289b12952783f8c355975ffe5316cf80
= <=>
2020-01-06 17:39:47 +0200

    Fix some stuff

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,18 +3,16 @@ import PropTypes from 'prop-types';
 import ResizeObserver from 'resize-observer-polyfill';
 import Photo, { photoPropType } from './Photo';
 import { computeColumnLayout } from './layouts/columns';
-import { computeRowLayout } from './layouts/justified';
-import { findIdealNodeSearch } from './utils/findIdealNodeSearch';
 
 const Gallery = React.memo(function Gallery({
   photos,
-  onClick,
   direction,
   margin,
   limitNodeSearch,
   targetRowHeight,
   columns,
   renderImage,
+  urlPath,
 }) {
   const [containerWidth, setContainerWidth] = useState(0);
   const galleryEl = useRef(null);
@@ -39,64 +37,26 @@ const Gallery = React.memo(function Gallery({
     };
   });
 
-  const handleClick = (event, { index }) => {
-    onClick(event, {
-      index,
-      photo: photos[index],
-      previous: photos[index - 1] || null,
-      next: photos[index + 1] || null,
-    });
-  };
-
   // no containerWidth until after first render with refs, skip calculations and render nothing
   if (!containerWidth) return <div ref={galleryEl}>&nbsp;</div>;
   // subtract 1 pixel because the browser may round up a pixel
   const width = containerWidth - 1;
   let galleryStyle, thumbs;
 
-  if (direction === 'row') {
-    // allow user to calculate limitNodeSearch from containerWidth
-    if (typeof limitNodeSearch === 'function') {
-      limitNodeSearch = limitNodeSearch(containerWidth);
-    }
-    if (typeof targetRowHeight === 'function') {
-      targetRowHeight = targetRowHeight(containerWidth);
-    }
-    // set how many neighboring nodes the graph will visit
-    if (limitNodeSearch === undefined) {
-      limitNodeSearch = 2;
-      if (containerWidth >= 450) {
-        limitNodeSearch = findIdealNodeSearch({ containerWidth, targetRowHeight });
-      }
-    }
+  columns = 2;
+  if (containerWidth >= 800) columns = 3;
 
-    galleryStyle = { display: 'flex', flexWrap: 'wrap', flexDirection: 'row' };
-    thumbs = computeRowLayout({ containerWidth: width, limitNodeSearch, targetRowHeight, margin, photos });
-  }
-  if (direction === 'column') {
-    // allow user to calculate columns from containerWidth
-    if (typeof columns === 'function') {
-      columns = columns(containerWidth);
-    }
-    // set default breakpoints if user doesn't specify columns prop
-    if (columns === undefined) {
-      columns = 1;
-      if (containerWidth >= 500) columns = 2;
-      if (containerWidth >= 900) columns = 3;
-      if (containerWidth >= 1500) columns = 4;
-    }
-    galleryStyle = { position: 'relative' };
-    thumbs = computeColumnLayout({ containerWidth: width, columns, margin, photos });
-    galleryStyle.height = thumbs[thumbs.length - 1].containerHeight;
-  }
+  galleryStyle = { position: 'relative' };
+  thumbs = computeColumnLayout({ containerWidth: width, columns, margin, photos });
+  galleryStyle.height = thumbs[thumbs.length - 1].containerHeight;
 
-  const renderComponent = renderImage || Photo;
+
   return (
     <div className="react-photo-gallery--gallery">
       <div ref={galleryEl} style={galleryStyle}>
         {thumbs.map((thumb, index) => {
           const { left, top, containerHeight, ...photo } = thumb;
-          return renderComponent({
+          return renderImage({
             left,
             top,
             key: thumb.key || thumb.src,
@@ -104,8 +64,8 @@ const Gallery = React.memo(function Gallery({
             index,
             margin,
             direction,
-            onClick: onClick ? handleClick : null,
             photo,
+            urlPath,
           });
         })}
       </div>
